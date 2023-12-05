cc2a93ff3c05d257af35258903c49af118ad4a76
Pxlart Design <michael.rhema@gmail.com>
2023-12-05 17:37:51 +0000

    zipped on Tue 05 Dec 2023 05:37:51 PM GMT

--- public/js-app/src/main.tsx
+++ public/js-app/src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-
-render(
+ReactDOM.createRoot(document.getElementById('get-code-button-container')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('get-code-button-container')!
 )
