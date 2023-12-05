4b2d5db42c9e6b29cef4022564a1bc9272cc9adb
Pxlart Design <michael.rhema@gmail.com>
2023-12-05 18:40:31 +0000

    zipped on Tue 05 Dec 2023 06:40:31 PM GMT

--- includes/class-get-code-activator.php
+++ includes/class-get-code-activator.php
@@ -32,32 +32,33 @@ class Get_Code_Activator
 	 */
 	public static function activate()
 	{
-		self::create_user_purchase_table();
+		create_user_purchase_table();
 	}
 
-	private function create_user_purchase_table()
-	{
-		global $wpdb;
+}
 
-		$table_name = $wpdb->prefix . GET_CODE_TABLE_NAME_USER_PURCHASES;
-		if ($wpdb->get_var($wpdb->prepare("SHOW TABLES LIKE %s", $table_name)) == $table_name) {
-			return;
-		}
-		$charset_collate = $wpdb->get_charset_collate();
-		// @todo: fix the lengths
-		$sql = "CREATE TABLE $table_name (
-			id mediumint(9) NOT NULL AUTO_INCREMENT,
-			user_id mediumint(9) NOT NULL,
-			post_url VARCHAR(255) DEFAULT NULL,
-			post_id VARCHAR(255) DEFAULT NULL,
-			code_tx_id VARCHAR(255) DEFAULT NULL,
-			tx_intent VARCHAR(255) DEFAULT NULL,
-			tx_status VARCHAR(20) DEFAULT NULL,
-			created_at datetime NOT NULL,
-			PRIMARY KEY  (id)
-		) $charset_collate;";
+function create_user_purchase_table()
+{
+	global $wpdb;
 
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		dbDelta($sql);
+	$table_name = $wpdb->prefix . GET_CODE_TABLE_NAME_USER_PURCHASES;
+	if ($wpdb->get_var($wpdb->prepare("SHOW TABLES LIKE %s", $table_name)) == $table_name) {
+		return;
 	}
+	$charset_collate = $wpdb->get_charset_collate();
+	// @todo: fix the lengths
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		user_id mediumint(9) NOT NULL,
+		post_url VARCHAR(255) DEFAULT NULL,
+		post_id VARCHAR(255) DEFAULT NULL,
+		code_tx_id VARCHAR(255) DEFAULT NULL,
+		tx_intent VARCHAR(255) DEFAULT NULL,
+		tx_status VARCHAR(20) DEFAULT NULL,
+		created_at datetime NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
 }
