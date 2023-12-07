f4e5728ad62c2252e4720a3e09050d47fc2a8704
Pxlart Design <michael.rhema@gmail.com>
2023-12-07 10:48:19 +0000

    zipped on Thu 07 Dec 2023 10:48:19 AM GMT

--- public/js-app/src/App.tsx
+++ public/js-app/src/App.tsx
@@ -1,12 +1,10 @@
 import CodeButton from './components/CodeButton';
-import { getCodeApp } from './getCodeApp';
 
 function App() {
 
-
   return (
     <>
-    <CodeButton amount={getCodeApp.default_amount || 0.5} destination={getCodeApp.destination || 'E8otxw1CVX9bfyddKu3ZB3BVLa4VVF9J7CTPdnUwT9jR'} />
+    <CodeButton />
     </>
   );
 }
--- public/js-app/src/components/CodeButton.tsx
+++ public/js-app/src/components/CodeButton.tsx
@@ -1,21 +1,29 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef } from 'react';
 import code from '@code-wallet/elements';
 import { handlePurchase } from '../common/fetch';
 
-type TCodeButton = {
-  destination: string
-  amount: number
+
+if (process.env.NODE_ENV != "production") {
+  window['GetCodeAppVars'] = {
+    "nonce": "644744b619",
+    "user_id": "0",
+    "ajax_url": "https://localhost:8898/wp-admin/admin-ajax.php",
+    "post_id": "417",
+    "destination": "E8otxw1CVX9bfyddKu3ZB3BVLa4VVF9J7CTPdnUwT9jR",
+    "default_amount": 0.4
+  };
 }
 
-function CodeButton({ destination, amount }: TCodeButton) {
+function CodeButton() {
   const el = useRef<HTMLDivElement>(null);
-  console.log(destination, amount)
+
   useEffect(() => {
     const { button } = code.elements.create('button', {
       currency: 'usd',
-      destination: destination,
-      amount: 0.5,
+      destination:  window['GetCodeAppVars'].destination,
+      amount:  window['GetCodeAppVars'].default_amount,
     });
 
     if (!button) return
@@ -27,7 +35,7 @@ function CodeButton({ destination, amount }: TCodeButton) {
 
     button.mount(el.current!);
 
-  }, [amount, destination]);
+  }, []);
 
   return (
     <>
