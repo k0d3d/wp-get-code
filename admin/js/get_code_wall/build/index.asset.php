e30331f8655d47c333d8c63bac511242e3b86b71
Pxlart Design <michael.rhema@gmail.com>
2023-12-12 00:39:38 +0000

    zipped on Tue 12 Dec 2023 12:39:38 AM GMT

--- admin/js/get_code_wall/build/index.asset.php
+++ admin/js/get_code_wall/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4378fb03af3c7f442533');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '384d137fb99cb3dad440');
