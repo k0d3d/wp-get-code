aae21665637c58d17290e24f04b259bc37fffa24
Pxlart Design <michael.rhema@gmail.com>
2024-05-18 16:54:58 +0000

    zipped on Sat 18 May 2024 04:54:58 PM GMT

--- includes/class-get-code.php
+++ includes/class-get-code.php
@@ -181,7 +181,7 @@ class Get_Code
 
 		$plugin_public = new Get_Code_Public($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action('wp_enqueue_styles', $plugin_public, 'enqueue_styles');
+		$plugin_public->enqueue_styles();
 		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
 	}
 
